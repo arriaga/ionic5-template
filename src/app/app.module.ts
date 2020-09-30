2e3d8b5072ecd1ed60fe9f284d8f733c4a252a70
luisarriaga <jorge.arriaga@gmail.com>
2020-09-30 15:28:36 -0500

    formato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,29 @@ import {HttpRequestInterceptor} from './interceptors/http-loading.interceptor';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OneSignal} from '@ionic-native/onesignal/ngx';
 import '@codetrix-studio/capacitor-google-auth';
 
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    HttpClientModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    OneSignal,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-      {
-          provide: HTTP_INTERCEPTORS,
-          useClass: HttpRequestInterceptor,
-          multi: true
-      }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+        HttpClientModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        OneSignal,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: HttpRequestInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
